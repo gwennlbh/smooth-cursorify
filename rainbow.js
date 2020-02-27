17f66bb386e4158f7c8097f3e0a8af8ea11eb9a9
Mx3 <ewen.lebihan7@gmail.com>
2020-02-27 13:16:35 +0100

    Update and rename rainbow.js to google-docs.js

--- rainbow.js
+++
@@ -1,3 +0,0 @@
-var cursor = document.getElementsByClassName("kix-cursor-caret")[0]
-var cursor_style = cursor.getAttribute("style");
-cursor.setAttribute('style', cursor_style+" border: 125% solid transparent; border-image: linear-gradient(to bottom, rgb(255, 0, 0) 0%, rgb(150, 0, 213) 60%, rgb(0, 78, 213) 61%, rgb(0, 78, 223) 125%); border-image-slice: 1;");
