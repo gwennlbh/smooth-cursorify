e4b2f8026b25dbef38362c661979979d76710152
Edwin <20777515+Lucien950@users.noreply.github.com>
2021-12-28 18:46:20 -0500

    Forgot to Initially Sync the Timing

--- google-docs.js
+++ google-docs.js
@@ -2,7 +2,10 @@ var cursor = document.getElementsByClassName("kix-cursor")[0]
 var cursor_style = cursor.getAttribute("style");
 
 // initial setting
-cursor.setAttribute('style', cursor_style + " transition: all 80ms ease;");
+chrome.storage.sync.get(['animationTime']).then(response => {
+    const val = response.animationTime ? response.animationTime : 80
+    cursor.setAttribute('style', cursor_style + " transition: all " + val + "ms ease;");
+})
 
 chrome.runtime.onMessage.addListener(function(request) {
     if (request.action == "change_cursor") {
