3d7b4bdeb22084b5a983306de6ab23889217e1f1
Jadua1010 <jmb-n@outlook.com>
2024-02-05 00:10:49 +0100

    Added google sheets

---
+++ jadua-drive.js
@@ -0,0 +1,3 @@
+var cursor = document.querySelector(".leaflet-cursor-container");
+var cursor_style = cursor.getAttribute("style");
+cursor.setAttribute('style', cursor_style+" transition: all 80ms;");
